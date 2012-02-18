1ec0cabb7be3a9a6af9a83fa46a0beecc642d964
David Larsen <dcl9934@cs.rit.edu>
2012-02-18 00:35:41 -0500

    Changed dynamic programming problem to Fibonacci.
    There doesn't seem to be a good dynamic programming problem which
    requires a two dimensional table, but doesn't take a significant amount
    of thought to solve.
    
    A dynamic programming (well, just memoization) approach to finding the
    n'th number in the Fibonacci sequence should be a much easier problem to
    digest while completing an exam.

---
+++ 242/Final/naive_fibonacci.py
@@ -0,0 +1,8 @@
+from ftrace import ftrace
+@ftrace
+def fib(n):
+    """Compute the n'th number in the Fibonacci sequence."""
+    if n in (0,1):
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
