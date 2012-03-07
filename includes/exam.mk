1bce51202970731ebb895a8a607c1ce37691d45f
David Larsen <dcl9934@cs.rit.edu>
2012-03-07 00:42:49 -0500

    Stop building DVI files.
    I don't find them useful for drafting anymore, since most PDF viewers
    now have the auto-update feature that xdvi does. Also, graphicx behaves
    differently when it's being run with latex or pdftex and since our
    published target is PDF, it makes sense to focus on that.

--- includes/exam.mk
+++ includes/exam.mk
@@ -1,16 +1,10 @@
 
 .PHONY: default clean realclean
 
-default: $(shell ls *.tex | sed 's/\(.*\)\.tex$$/\1.dvi \1.pdf/')
-
-%-ANSWERS.dvi: %-ANSWERS.tex %.tex
-	latex $<
+default: $(shell ls *.tex | sed 's/^\(.*\)\.tex$$/\1.pdf/')
 
 %-ANSWERS.pdf: %-ANSWERS.tex %.tex 
 	pdflatex $<
-
-%.dvi: %.tex
-	latex $<
 
 %.pdf: %.tex
 	pdflatex $<
