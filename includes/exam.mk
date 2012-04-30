f17e2ae0a1868b381564a8d320a05b2f6ffd08f1
Adam Lohr <masterpi@masterpi.org>
2012-04-29 21:03:16 -0400

    Added csc package & logos to 242/243 reviews
    Signed-off-by: David Larsen <dcl9934@cs.rit.edu>

--- includes/exam.mk
+++ includes/exam.mk
@@ -1,13 +1,19 @@
+INCLUDEPATH := $(dir $(lastword $(MAKEFILE_LIST)))
 
 .PHONY: default clean realclean
 
 default: $(shell ls *.tex | sed 's/^\(.*\)\.tex$$/\1.pdf/')
 
-%-ANSWERS.pdf: %-ANSWERS.tex %.tex 
+export TEXINPUTS := $(TEXINPUTS):$(INCLUDEPATH)
+
+INCLUDEDEPS:=$(INCLUDEPATH)/csclogo.pdf\
+	     $(INCLUDEPATH)/csc.sty
+
+%-ANSWERS.pdf: %-ANSWERS.tex %.tex $(INCLUDEDEPS)
 	pdflatex $<
 	pdflatex $<
 
-%.pdf: %.tex
+%.pdf: %.tex $(INCLUDEDEPS)
 	pdflatex $<
 	pdflatex $<
 
