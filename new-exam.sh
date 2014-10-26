11bf39a30339df5313ae20c7a6cd0cfd1151593f
Dakota Williams <drw9888@rit.edu>
2014-10-26 15:32:44 -0400

    template (spooky) skeleton

---
+++ new-exam.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage ./new-exam.sh <class name> <exam name>"
+	exit 1
+fi
+
+month=`date +%m`
+year=`date +%Y`
+
+if [ "$year" -lt 6 ]; then
+	$[ year-- ]
+fi
+
+mkdir "$1"
+cp "template.tex" "$1/CSC-$1-$2-$year-Review.tex"
+cp "template-ANSWERS.tex" "$1/CSC-$1-$2-$year-Review-ANSWERS.tex"
+mkdir "$1/questions"
+touch "$1/questions/delete-me"
+cp "template-makefile" "$1/Makefile"
+echo "\\input{CSC-$1-$2-$year-Review.tex}" >> "$1/CSC-$1-$2-$year-Review-ANSWERS.tex"
