011f9c1a467d5db5a132484a62dcfed72c8ed824
Chris Lydic <cxl9041@rit.edu>
2017-11-06 11:00:38 -0500

    Fixed CS1 exam 2 review questions for Fall 2017 -- new rit lib, no hashing, different linked list

--- 141/code/hotel.py
+++ 141/code/hotel.py
@@ -1,13 +1,7 @@
 from rit_lib import *
 
-class Hotel( struct ):
-	_slots = ( (str,"name"), (list,"rooms"), (str,"location") )
+Hotel = struct_type("Hotel",
+	(str, "name"), (list, "rooms"), (str, "location"))
 
-def createHotel(name, rooms, location):
-	return Hotel(name, rooms, location)
-
-class Room( struct ):
-	_slots = ( (int,"number"), (int,"capacity"), (float,"price") )
-
-def createRoom(number, capacity, price):
-	return Room(number, capacity, price)
+Room = struct_type("Room",
+	(int, "number"), (int, "capacity"), (float, "price"))
